069c6afcf894612ce778ae1bf63a63a922c8a59f
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2024-09-11 13:17:21 -0300

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+   <x-slot:heading>
+    Contact Page
+   </x-slot:heading>
+   
+   <h1>Contact content</h1>
+   
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->simplePaginate(3);
+
+    return view('jobs', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+    dd('hello there');
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
